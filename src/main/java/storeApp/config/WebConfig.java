a46af9768ae63291a20a47fa113ab7fcb0b7b9e7
Roberto Cruz <cr439694@uaeh.edu.mx>
2024-11-14 21:27:51 -0600

    deploy cors global fix?

---
+++ src/main/java/storeApp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package storeApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://top50tracker.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
